4afc2c59414f301804dc942ec49956896127afbe
milkaalinka <alina.shumilova1704@gmail.com>
2022-06-19 20:02:44 +0300

    changed adapter for recycleView

--- app/src/main/java/com/alinaincorporated/diploma/ui/transactions/Adapter.kt
+++ app/src/main/java/com/alinaincorporated/diploma/ui/transactions/Adapter.kt
@@ -5,39 +5,33 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.alinaincorporated.diploma.R
 import com.alinaincorporated.diploma.database.TransactionEntity
+import com.alinaincorporated.diploma.databinding.ActivityMainBinding.inflate
+import com.alinaincorporated.diploma.databinding.FragmentAddTransactionBinding.inflate
+import com.alinaincorporated.diploma.databinding.ListItemViewBinding
+import com.alinaincorporated.diploma.databinding.ListItemViewBinding.inflate
 
-class TransactionAdapter(private val transaction: List<TransactionEntity>) :
-    RecyclerView.Adapter<TransactionAdapter.ViewHolder>() {
+class TransactionAdapter(private val transactionList: List<TransactionEntity>) : RecyclerView.Adapter<TransactionAdapter.TransactionHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionHolder {
+        val itemBinding = ListItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TransactionHolder(itemBinding)
+    }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textViewCategory: TextView
-        val textViewDate: TextView
-        val textViewAmount: TextView
+    override fun onBindViewHolder(holder: TransactionHolder, position: Int) {
+        val transactionBean: TransactionEntity = transactionList[position]
+        holder.bind(transactionBean)
+    }
 
-        init {
-            textViewCategory = view.findViewById(R.id.transactTextName)
-            textViewDate = view.findViewById(R.id.transactDate)
-            textViewAmount = view.findViewById(R.id.transactAmount)
+    override fun getItemCount(): Int = transactionList.size
+
+    class TransactionHolder(private val itemBinding: ListItemViewBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(transactionBean: TransactionEntity) {
+            itemBinding.transactTextName.text = transactionBean.categoryId.toString()
+            itemBinding.transactDate.text = transactionBean.dateTime.toString()
+            itemBinding.transactAmount.text = transactionBean.amount.toString()
         }
     }
-
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.list_item_view, viewGroup, false)
-
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-        viewHolder.textViewCategory.text = transaction[position].toString()
-        viewHolder.textViewDate.text = transaction[position].toString()
-        viewHolder.textViewAmount.text = transaction[position].toString()
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = transaction.size
-
 }
+
